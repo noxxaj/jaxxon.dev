579ef623928ad66399f9136732b11caa13ef02b5
jaxxon alta <jaxxonalta@gmail.com>
2023-01-26 14:53:44 +0100

    added image urls to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_194712) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_135308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_194712) do
     t.text "description", default: "Info about the project"
     t.string "language"
     t.string "category"
+    t.string "image_url"
   end
 
 end
