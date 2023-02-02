739c398d07647b2ca4901d71d76790480120277f
jaxxon alta <jaxxonalta@gmail.com>
2023-02-02 19:56:08 +0100

    routes working, projects clickable and identified by name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,18 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only: :show
+
   def index
     @projects = Project.all.order(name: :desc)
   end
 
   def show
-
+    @project = Project.find_by(name: params[:name])
   end
 
   private
 
   def set_project
-    @project = Project.find(params[:id])
+    @project = Project.find_by(name: params[:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
+  resources :projects, only: %i[index show], param: :name
+
   get '/projects/:name', to: 'projects#show', as: 'project'
   get '/projects', to: 'projects#index'
   get '/about', to: 'pages#about'
